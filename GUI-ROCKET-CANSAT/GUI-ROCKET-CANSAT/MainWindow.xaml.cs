7534b89c7674273b99d802d4e4e50cf35a3e85e8
Leonard Felix Sadewa Siahilan <leonard.felix01@ui.ac.id>
2021-10-11 16:12:45 +0700

    updated design

--- GUI-ROCKET-CANSAT/GUI-ROCKET-CANSAT/MainWindow.xaml.cs
+++ GUI-ROCKET-CANSAT/GUI-ROCKET-CANSAT/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GUI_ROCKET_CANSAT
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
